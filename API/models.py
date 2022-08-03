23a2e7d650a0e4ee5a1c90f61e8b2c334b7fa8f2
Brandon D <brandondmcr2020@gmail.com>
2022-08-03 18:53:38 +0100

    Basic rest endpoints for user model

--- API/models.py
+++ API/models.py
@@ -1,51 +1,66 @@
 import uuid
 
-from uuid import UUID
-from sqlalchemy import Boolean, Column, Date, ForeignKey, Integer, String, Table
+from sqlalchemy import Boolean, Column, Date, ForeignKey, Integer, String, Table, UniqueConstraint
 from sqlalchemy.orm import relationship
 
-from .database import Base
+from database import Base
 
-users_events = Table('user_booking',
-                        Column('user_id', UUID(as_uuid=True),
-                                  ForeignKey('user.id')),
-                        Column('booking_id', UUID(as_uuid=True),
-                                  ForeignKey('booking.id'))
-                        )
+users_teams = Table('user_team',
+                    Base.metadata,
+                    Column('user_id', Integer,
+                           ForeignKey('users.id')),
+                    Column('team_id', Integer,
+                           ForeignKey('teams.id'))
+                    )
+
+teams_desks = Table('teams_desk',
+                    Base.metadata,
+                    Column('team_id', Integer,
+                           ForeignKey('teams.id')),
+                    Column('desk_id', Integer,
+                           ForeignKey('desks.id'))
+                    )
+
 
 class User(Base):
     __tablename__ = "users"
-    
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    username = Column(String, unique=True, nullable=False)
-    hashed_password = Column(String(128))
-    team_id = Column(UUID(as_uuid=True), ForeignKey(
-        'team.id'), unique=False, nullable=True)
 
-    
+    # Possibly want to do UUID/GUID for users :)
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    name = Column(String, unique=False, nullable=False)
+    hashed_password = Column(String(128))
+
+
 class Team(Base):
     __tablename__ = "teams"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True, nullable=False)
-    members = relationship('User', backref='team')
+
 
 class Desk(Base):
     __tablename__ = "desks"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    number = Column(Integer, unique=True, nullable=False)
-    assigned_team = Column(UUID(as_uuid=True), ForeignKey(
-        'team.id'), unique=False, nullable=True)
+    id = Column(Integer, primary_key=True, index=True)
+    desk_number = Column(Integer, nullable=False)
+    room_name = Column(String, nullable=False)
+    assigned_team = Column(Integer, ForeignKey(
+        'teams.id'), unique=False, nullable=True)
+
+    __table_args__ = (UniqueConstraint(
+        'desk_number', 'room_name', name='_desk_room_uc'),
+    )
 
 
 class Booking(Base):
     __tablename__ = "bookings"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    id = Column(Integer, primary_key=True, index=True)
     approved_status = Column(Boolean, unique=False, nullable=False)
     start_date = Column(Date, unique=False, nullable=False)
     end_date = Column(Date, unique=False, nullable=False)
-    desk_id = Column(UUID(as_uuid=True), ForeignKey(
-        'desk.id'), unique=False, nullable=False)
-    users = relationship('User', secondary=users_events, backref='events')
+    user_id = Column(Integer, ForeignKey(
+        'users.id'), unique=False, nullable=False)
+    desk_id = Column(Integer, ForeignKey(
+        'desks.id'), unique=False, nullable=False)
--- API/schemas.py
+++ API/schemas.py
@@ -1,57 +1,85 @@
-from typing import Union
+from typing import Union, List
 from uuid import UUID
 import datetime
 from pydantic import BaseModel
 
+
 class UserBase(BaseModel):
-    username: str
-    
+    name: str
+    email: str
+
+
 class UserCreate(UserBase):
     password: str
 
+
 class User(UserBase):
-    id: UUID
-    team_id: UUID
+    id: int
 
     class Config:
         orm_mode = True
+
 
 class TeamBase(BaseModel):
     name: str
-    
+
+
 class TeamCreate(TeamBase):
     pass
 
+
 class Team(TeamBase):
-    id: UUID
+    id: int
 
     class Config:
         orm_mode = True
+
 
 class DeskBase(BaseModel):
-    number: int
-    
+    desk_number: int
+    room_name: str
+    assigned_team: int
+
+
 class DeskCreate(DeskBase):
-    assigned_team: UUID
+    pass
+
 
 class Desk(DeskBase):
-    id: UUID
+    id: int
 
     class Config:
         orm_mode = True
-    
+
+
 class BookingBase(BaseModel):
     approved_status: str
     start_date: datetime.date
     end_date: datetime.date
-    desk_id: UUID
-    
+
+
 class BookingCreate(BookingBase):
     pass
 
+
 class Booking(BookingBase):
-    id: UUID
+    id: int
 
     class Config:
         orm_mode = True
 
+
+class UserSchema(UserBase):
+    authors: List[TeamBase]
+
+
+class UTeamSchema(TeamBase):
+    books: List[UserBase]
+
+
+class DTeamSchema(TeamBase):
+    authors: List[DeskBase]
+
+
+class DeskSchema(DeskBase):
+    books: List[DeskBase]
