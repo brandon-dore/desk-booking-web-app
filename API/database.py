94885258ae38eedfac8a622aad13ba8a8369e676
Brandon D <brandondmcr2020@gmail.com>
2022-08-05 16:53:00 +0100

    Turn on debugging

--- API/database.py
+++ API/database.py
@@ -5,8 +5,10 @@ from sqlalchemy.orm import sessionmaker
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost:5432/desk_booking_db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
+    SQLALCHEMY_DATABASE_URL,
+    echo=True
 )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
